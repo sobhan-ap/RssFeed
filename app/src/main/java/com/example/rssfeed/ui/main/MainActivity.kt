7b4d997b064ad9fd3b012b56252513a59b27b858
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-10 00:08:18 +0430

    Main Activity -> - add Bottom navigation to .kt and xml files

---
+++ app/src/main/java/com/example/rssfeed/ui/main/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.rssfeed.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.rssfeed.R
+import com.example.rssfeed.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private val binding
+        get() = _binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bnv_main)
+            .setupWithNavController(navController = navController)
+    }
+}
