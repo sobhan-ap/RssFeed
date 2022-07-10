d6f9ee593d5675dfa5353a6381f5a86962f18ead
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-10 22:41:30 +0430

    add getBitcoinNews Fun

--- app/src/main/java/com/example/rssfeed/data/network/ApiService.kt
+++ app/src/main/java/com/example/rssfeed/data/network/ApiService.kt
@@ -3,11 +3,15 @@ package com.example.rssfeed.data.network
 import com.example.rssfeed.data.model.News
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET
+    @GET("everything")
     suspend fun getBitCoinNewsList(
-
+        @Query(value = "q") query: String,
+        @Query(value = "apiKey") apiKey: String,
+        @Query(value = "pageSize") pageSize: Int,
+        @Query(value = "page") page: Int,
     ): Response<News>
 }
