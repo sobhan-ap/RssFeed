3c16e4df711467c9fa4cd402401cebcfa5b8152c
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-10 20:55:13 +0430

    add NetworkResult class

---
+++ app/src/main/java/com/example/rssfeed/data/network/NetworkResult.kt
@@ -0,0 +1,10 @@
+package com.example.rssfeed.data.network
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : NetworkResult<T>(data = data)
+    class Error<T>(errorMessage: String) : NetworkResult<T>(message = errorMessage)
+    class Loading<T> : NetworkResult<T>()
+}
