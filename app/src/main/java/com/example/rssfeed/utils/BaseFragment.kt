1b44066b56e5b0c9dbb9413b7bfd35a556300283
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-10 22:40:11 +0430

    add BaseFragment

---
+++ app/src/main/java/com/example/rssfeed/utils/BaseFragment.kt
@@ -0,0 +1,39 @@
+package com.example.rssfeed.utils
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+
+    private var _binding: T? = null
+    val binding: T
+        get() = _binding!!
+
+    @get:LayoutRes
+    protected abstract val layoutId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            layoutId,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
