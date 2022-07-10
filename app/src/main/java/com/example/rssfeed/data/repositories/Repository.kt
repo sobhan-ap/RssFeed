fb618bbaec89dbbe4499e82bd4c72cef3e5c9d41
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-10 22:40:51 +0430

    add Repository

---
+++ app/src/main/java/com/example/rssfeed/data/repositories/Repository.kt
@@ -0,0 +1,32 @@
+package com.example.rssfeed.data.repositories
+
+import com.example.rssfeed.data.model.News
+import com.example.rssfeed.data.network.NetworkResult
+import com.example.rssfeed.datasources.LocalDataSource
+import com.example.rssfeed.datasources.RemoteDataSource
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Repository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource
+) {
+    suspend fun getBitcoinNewsRemote(page: Int): NetworkResult<News> {
+        return when (val result = remoteDataSource.getBitcoinList(page)) {
+            is NetworkResult.Success ->
+                try {
+                    NetworkResult.Success(
+                        data = result.data!!
+                    )
+                } catch (e: Exception) {
+                    NetworkResult.Error(e.message ?: "UnhandledException")
+                }
+            is NetworkResult.Error ->
+                NetworkResult.Error(result.message ?: "UnhandledException")
+            else -> {
+                NetworkResult.Error("NetworkError")
+            }
+        }
+    }
+}
