9c0b4f49fa47f166f8107733058de0434c1bca02
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:24:34 +0430

    add Extension Functions

---
+++ app/src/main/java/com/example/rssfeed/utils/Extensions.kt
@@ -0,0 +1,68 @@
+package com.example.rssfeed.utils
+
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+
+fun AppCompatImageView.loadImageWithGlide(url: String?) {
+    Glide.with(this)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
+}
+
+fun AppCompatImageView.loadImageWithGlideMediumRadius(url: String?) {
+    Glide.with(this)
+        .load(url)
+        .apply(GlideExtensions.mediumRadius(context))
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
+}
+
+fun Activity.showLongToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.showLongToast(resourceId: Int) {
+    Toast.makeText(this, resourceId, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.showShortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.showShortToast(resourceId: Int) {
+    Toast.makeText(this, resourceId, Toast.LENGTH_SHORT).show()
+}
+
+fun RecyclerView.addVerticalDividerSpacing16(
+    context: Context,
+    showFirstDivider: Boolean = false,
+    showLastDivider: Boolean = true
+) {
+    if (itemDecorationCount == 0) {
+
+        val divider = DividerItemDecoration.verticalDividerSpacing16(context)
+
+        if (showFirstDivider)
+            divider.showFirstDivider()
+
+        if (showLastDivider)
+            divider.showLastDivider()
+
+        divider.build().addTo(this)
+    }
+}
+
+fun NestedScrollView.scrollToTop() {
+    smoothScrollTo(0, 0)
+}
+
+fun RecyclerView.scrollToTop() {
+    smoothScrollToPosition(0)
+}
