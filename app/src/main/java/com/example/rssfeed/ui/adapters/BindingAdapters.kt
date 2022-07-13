7529d86fa96b9bc8d48b1cb6b22a9895ceba4865
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:23:26 +0430

    add binding adapters

---
+++ app/src/main/java/com/example/rssfeed/ui/adapters/BindingAdapters.kt
@@ -0,0 +1,35 @@
+package com.example.rssfeed.ui.adapters
+
+import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.example.rssfeed.utils.loadImageWithGlideMediumRadius
+
+@BindingAdapter("setImageUrlMediumRadius")
+fun setImageUrlMediumRadius(imageView: AppCompatImageView, url: String?) {
+    imageView.loadImageWithGlideMediumRadius(url)
+}
+
+@BindingAdapter("setBackground")
+fun setBackground(view: View, background: Int) {
+    if (background != 0) {
+        val drawable = AppCompatResources.getDrawable(view.context, background)
+        view.background = drawable
+    }
+}
+
+@BindingAdapter("setVisibilityGone")
+fun setVisibilityGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("setVisibilityInvisible")
+fun setVisibilityInvisible(view: View, isInvisible: Boolean) {
+    view.visibility = if (isInvisible) View.INVISIBLE else View.VISIBLE
+}
+
+@BindingAdapter("setDisable")
+fun setDisable(view: View, isLoading: Boolean) {
+    view.isEnabled = !isLoading
+}
