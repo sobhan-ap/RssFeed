0f566f9a1ddddb965e62672874b869db84d1f403
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:27:32 +0430

    add base urls

--- app/src/main/java/com/example/rssfeed/utils/Constants.kt
+++ app/src/main/java/com/example/rssfeed/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.rssfeed.utils
 
-
-const val FEED_TABS_NUMBER: Int = 2
+internal const val BASE_URL: String = "https://newsapi.org/v2/"
+internal const val XML_BASE_URL = "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
+internal const val FEED_TABS_NUMBER: Int = 2
 
