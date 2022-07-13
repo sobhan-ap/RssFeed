f4f0f207ba09c3677d63a232c7c2a9f04f07b558
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:31:43 +0430

    change Fragments

--- app/src/main/java/com/example/rssfeed/ui/adapters/FeedTabsAdapter.kt
+++ app/src/main/java/com/example/rssfeed/ui/adapters/FeedTabsAdapter.kt
@@ -2,13 +2,18 @@ package com.example.rssfeed.ui.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.rssfeed.ui.feed.news.NewsFragment
+import com.example.rssfeed.ui.feed.news.JsonNewsFragment
+import com.example.rssfeed.ui.feed.news.XmlNewsFragment
 
 class FeedTabsAdapter(fragment: Fragment, private val count: Int) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = count
 
     override fun createFragment(position: Int): Fragment {
-        return NewsFragment()
+        return when (position) {
+            0 -> JsonNewsFragment()
+            1 -> XmlNewsFragment()
+            else -> throw Exception()
+        }
     }
 }
