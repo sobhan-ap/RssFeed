108aa186e6da449fdcad8fb5accfdd79a8c973ac
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:41:16 +0430

    add Gson change BaseUrl location

--- app/src/main/java/com/example/rssfeed/di/NetworkModule.kt
+++ app/src/main/java/com/example/rssfeed/di/NetworkModule.kt
@@ -1,6 +1,9 @@
 package com.example.rssfeed.di
 
 import com.example.rssfeed.data.network.ApiService
+import com.example.rssfeed.utils.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,22 +11,29 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL: String = "https://newsapi.org/v2/"
-
     @Singleton
     @Provides
     fun providesHttpLoggingInterceptor(): HttpLoggingInterceptor =
-        HttpLoggingInterceptor().apply {
+        HttpLoggingInterceptor{ message ->
+            //show logs
+        }.apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return GsonBuilder().create()
+    }
 
     @Singleton
     @Provides
@@ -35,19 +45,18 @@ object NetworkModule {
             .connectTimeout(30L, TimeUnit.SECONDS)
             .build()
 
-
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(okHttpClient)
             .build()
 
     @Singleton
     @Provides
-    fun provideApiService(retrofit: Retrofit): ApiService =
-        retrofit.create(ApiService::class.java)
-
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
 }
