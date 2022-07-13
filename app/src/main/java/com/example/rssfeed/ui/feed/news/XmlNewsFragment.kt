6d03008cadb7871a0eacdffd3103c1f54857b337
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-14 00:43:51 +0430

    add xmlNews methods

---
+++ app/src/main/java/com/example/rssfeed/ui/feed/news/XmlNewsFragment.kt
@@ -0,0 +1,57 @@
+package com.example.rssfeed.ui.feed.news
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.example.rssfeed.R
+import com.example.rssfeed.data.network.NetworkResult
+import com.example.rssfeed.databinding.FragmentXmlNewsBinding
+import com.example.rssfeed.ui.adapters.ArticleListAdapter
+import com.example.rssfeed.ui.feed.FeedViewModel
+import com.example.rssfeed.utils.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class XmlNewsFragment : BaseFragment<FragmentXmlNewsBinding>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_xml_news
+
+    private val _viewModel by viewModels<FeedViewModel>()
+    private lateinit var _articleAdapter: ArticleListAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        _viewModel.getXmlArticle.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is NetworkResult.Success -> {
+                    binding.refreshLayout.isRefreshing = false
+                }
+                is NetworkResult.Loading -> {
+                    binding.refreshLayout.isRefreshing = true
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(requireContext(), "XmlNewsError", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        _articleAdapter = ArticleListAdapter {
+            //TODO navigation to WebView
+        }
+        binding.rvNewsList.adapter = _articleAdapter
+    }
+}
