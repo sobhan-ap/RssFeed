5e7b37c69afcd63f6cd42613a09c0f1ae6a81c01
SobhanAlipanah <sobhan.ap09@gmail.com>
2022-07-16 18:28:52 +0430

    add Bundle Key String

--- app/src/main/java/com/example/rssfeed/utils/Constants.kt
+++ app/src/main/java/com/example/rssfeed/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.rssfeed.utils
 internal const val BASE_URL: String = "https://newsapi.org/v2/"
 internal const val XML_BASE_URL = "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
 internal const val FEED_TABS_NUMBER: Int = 2
+internal const val WEB_URL_ARG = "webUrl"
 
